2baf78b0a0a3ca36676e861e20e10507dbf848c1
PFC(Ian) <pfc-validator@protonmail.com>
2021-09-16 19:46:45 -0500

    cleanup a bit

--- crates/discord/src/task.rs
+++ crates/discord/src/task.rs
@@ -5,6 +5,7 @@ use actor_discord::DiscordAPI;
 use actor_discord::DiscordBot;
 use actor_discord::GatewayIntents;
 use constellation_shared::state::AppState;
+use tokio::time::Duration;
 
 pub async fn run(_state: AppState, discord_token: String, discord_url: String, max_retries: usize) {
     let intents: GatewayIntents = GatewayIntents::GUILDS
@@ -12,14 +13,18 @@ pub async fn run(_state: AppState, discord_token: String, discord_url: String, m
         | GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::GUILD_MESSAGE_REACTIONS
         | GatewayIntents::DIRECT_MESSAGE_REACTIONS;
-    match DiscordAPI::create(&discord_token, &discord_url, max_retries) {
-        Ok(discord_api) => match DiscordBot::create(&discord_api, intents).await {
-            Ok(mut discord_bot) => match discord_bot.start_websocket().await {
-                Ok(_) => {}
-                Err(e) => log::error!("Error:{}", e),
+    loop {
+        match DiscordAPI::create(&discord_token, &discord_url, max_retries) {
+            Ok(discord_api) => match DiscordBot::create(&discord_api, intents).await {
+                Ok(mut discord_bot) => match discord_bot.start_websocket().await {
+                    Ok(_) => {}
+                    Err(e) => log::error!("Error websocket:{}", e),
+                },
+                Err(e) => log::error!("Error creating bot:{}", e),
             },
-            Err(e) => log::error!("Error {}", e),
-        },
-        Err(e) => log::error!("Error:{}", e),
-    };
+            Err(e) => log::error!("Error creatung api:{}", e),
+        };
+        log::warn!("Discord Disconnected..waiting 2 secs and trying again");
+        tokio::time::sleep(Duration::from_secs(2)).await;
+    }
 }
